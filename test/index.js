179a16e3d1a40afd1de12a0702090c2d196f2d57
Wei Lu <luwei.here@gmail.com>
2014-08-22 10:21:43 +0800

    extend test timeout for travis

--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ var request = require('request')
 var Blockchain = require('../src/index.js')
 
 describe('Blockchain API', function() {
-  this.timeout(5000)
+  this.timeout(8000)
 
   var blockchain
 
