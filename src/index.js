19b215a3945c6abd540c82bbddef3a51adb35bad
Daniel Cousens <github@dcousens.com>
2014-12-03 11:30:05 +1100

    index: expose internal types, add error for unknown network

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+var assert = require('assert')
+
 var Addresses = require('./addresses')
 var Blocks = require('./blocks')
 var Transactions = require('./transactions')
@@ -10,12 +12,21 @@ var NETWORKS = {
 
 function Blockr(network) {
   network = network || 'bitcoin'
+  assert(network in NETWORKS, 'Unknown network: ' + network)
   var BASE_URL = 'https://' + NETWORKS[network] + '.blockr.io/api/v1/'
 
   // end points
   this.transactions = new Transactions(BASE_URL + 'tx/')
   this.addresses = new Addresses(BASE_URL + 'address/', this.transactions)
   this.blocks = new Blocks(BASE_URL + 'block/', this.transactions)
+
+  this.network = network
 }
+
+Blockr.Addresses = Addresses
+Blockr.Blocks = Blocks
+Blockr.Transactions = Transactions
+
+Blockr.prototype.getNetwork = function() { return this.network }
 
 module.exports = Blockr
